3a7ec2cfbac4d6c18e7feeabca8089433f49afd6
sjpence <sjpence00@gmail.com>
2015-09-18 01:09:11 -0400

    Create initial project

---
+++ wd-rencard/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="wd_rencard.MvcApplication" Language="C#" %>
---
+++ wd-rencard/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(wd_rencard.Startup))]
+namespace wd_rencard
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
